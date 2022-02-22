0dbb8b1779951a361dd9e9d13e06eaf05b9372c9
Jose Esquinca <esquinca_jose@hotmail.com>
2022-02-22 15:21:29 -0500

    search funcion hibernate

--- src/main/java/com/spring/mvc/controller/ProductController.java
+++ src/main/java/com/spring/mvc/controller/ProductController.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -77,16 +80,41 @@ public class ProductController {
 	@GetMapping("/search")
 	public String buscar(@ModelAttribute("search") Product product, Model model) {
 		System.out.println("search by ss : " + product);
-		List<Product> productsList = productService.getProductList();
+		//List<Product> productsList = productService.getProductList();
+		List<Product> productsList = productService.getProductsHibernate();
 		List<Product> productsFilter = productsList.stream()
 				.filter(pro -> pro.getProductName().contains(product.getProductName())).collect(Collectors.toList());
 
 		System.out.println("filtering");
 		productsFilter.forEach(x -> System.out.println(x));
-
 		ProductDto productDto = new ProductDto(productsFilter);
 		model.addAttribute("productDto", productDto);
-
+		/*SessionFactory sFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Product.class)
+				.buildSessionFactory();
+		Session session = sFactory.openSession();
+		List<Product> results = null;
+		try {
+			session.beginTransaction();
+			
+			String hql = "FROM Product Where productName like :search";
+			System.out.println("este es el query: " + hql);	
+			//Query query = session.createQuery(hql);
+			Query query = session.createQuery(hql);  
+			query.setParameter("search", "%" + product.getProductName() + "%" );
+			//List list=query.list();
+			
+			results = query.getResultList();
+			model.addAttribute("productDto", results);
+			
+			session.close();
+			sFactory.close();
+			
+		} catch (Exception e) {
+			System.out.println("se genero un error");
+			e.printStackTrace();
+		}*/
+		
+		
 		return "/productView";
 	}
 
